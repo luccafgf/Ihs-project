b56158e75690fcb2389eb4bec734c3f56c64505b
Heitorado <hscs@cin.ufpe.br>
2019-06-07 17:01:54 -0300

    Melhoramento do app.c com funcao para mudança de display

--- App/app.c
+++ App/app.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <omp.h>
 #include <time.h>
 
 
@@ -12,24 +13,119 @@
 #define SEVEN_DISPLAYS_4    5
 #define SEVEN_DISPLAYS_2    6
 
-unsigned char hexdigit[] = {0x3F, 0x06, 0x5B, 0x4F,
-							0x66, 0x6D, 0x7D, 0x07, 
-                            0x7F, 0x6F, 0x77, 0x7C,
-			                0x39, 0x5E, 0x79, 0x71};
+#define DISPLAY_HEX0        0
+#define DISPLAY_HEX1        1
+#define DISPLAY_HEX2        2
+#define DISPLAY_HEX3        3
+#define DISPLAY_HEX4        4
+#define DISPLAY_HEX5        5
+#define DISPLAY_HEX6        6
+#define DISPLAY_HEX7        7
+
+#define DISPLAY_ZERO        64
+#define DISPLAY_ONE    		121
+#define DISPLAY_TWO			36
+#define DISPLAY_THREE		48
+#define DISPLAY_FOUR		25
+#define DISPLAY_FIVE		18
+#define DISPLAY_SIX			2
+#define DISPLAY_SEVEN 		7
+#define DISPLAY_EIGHT		0
+#define DISPLAY_NINE 		16
+#define DISPLAY_A           8
+#define DISPLAY_B			3
+#define DISPLAY_C           70
+#define DISPLAY_D			33
+#define DISPLAY_E 			6
+#define DISPLAY_F 			14
+
+void changeDisplay(int altera, int display, uint32_t value);
+
+void delay(int num_of_mili) {
+	int mili_sec = 1000*num_of_mili;
+	clock_t start_time = clock();
+	while(clock() < start_time + mili_sec);
+} 
 
 int main() {
-  int i, j, k;
-
   
   int altera = open("/dev/de2i150_altera", O_RDWR);
   
-  const uint32_t led_1 = 3, led_2 = 82222888, led_3 = 142545, led_4 = 192;
+  const uint32_t led_1 = 3, led_2 = 65535, led_3 = 142545, led_4 = 192;
 
-  write(altera, &led_2, 3);
+  changeDisplay(altera, DISPLAY_HEX0, DISPLAY_A);
+  delay(100);
+  changeDisplay(altera, DISPLAY_HEX1, DISPLAY_B);
 
-  write(altera, &led_2, 4);  
-  
   close(altera);
   return 0;
+}
+
+void setup() {	
+
+}
+
+void start() {
+
+} 
+
+void changeDisplay(int altera, int display, uint32_t value) {
+
+	uint32_t actualValue = 0;
+
+	switch(display) {
+	case DISPLAY_HEX0:
+		read(altera, &actualValue, SEVEN_DISPLAYS_4);
+		actualValue = ((actualValue & 0xffffff00) | value);
+		write(altera, &actualValue, SEVEN_DISPLAYS_4);
+		delay(100);
+		break;
+	case DISPLAY_HEX1:
+		read(altera, &actualValue, SEVEN_DISPLAYS_4);
+		actualValue = ((actualValue | 0xffff00ff) & (value << 8));
+		write(altera, &actualValue, SEVEN_DISPLAYS_4);
+		delay(100);
+		break;
+	case DISPLAY_HEX2:
+		read(altera, &actualValue, SEVEN_DISPLAYS_4);
+		actualValue = ((actualValue & 0xff00ffff) | (value << 16));
+		write(altera, &actualValue, SEVEN_DISPLAYS_4);
+		delay(100);
+		break;
+	case DISPLAY_HEX3:
+		read(altera, &actualValue, SEVEN_DISPLAYS_4);
+		actualValue = ((actualValue & 0x00ffffff) | (value << 24));
+		write(altera, &actualValue, SEVEN_DISPLAYS_4);
+		delay(100);
+		break;
+
+	case DISPLAY_HEX4:
+		read(altera, &actualValue, SEVEN_DISPLAYS_2);
+		actualValue = ((actualValue & 0xffffff00) | value);
+		write(altera, &actualValue, SEVEN_DISPLAYS_2);
+		delay(100);
+		break;
+	case DISPLAY_HEX5:
+		read(altera, &actualValue, SEVEN_DISPLAYS_2);
+		actualValue = ((actualValue & 0xffff00ff) | (value << 8));
+		write(altera, &actualValue, SEVEN_DISPLAYS_2);
+		delay(100);
+		break;
+	case DISPLAY_HEX6:
+		read(altera, &actualValue, SEVEN_DISPLAYS_2);
+		actualValue = ((actualValue & 0xff00ffff) | (value << 16));
+		write(altera, &actualValue, SEVEN_DISPLAYS_2);		
+		delay(100);
+		break;
+	case DISPLAY_HEX7:
+		read(altera, &actualValue, SEVEN_DISPLAYS_2);
+		actualValue = ((actualValue & 0x00ffffff) | (value << 24));
+		write(altera, &actualValue, SEVEN_DISPLAYS_2);
+		delay(100);
+		break;
+	default:
+		break;
+	}
+
 }
 
